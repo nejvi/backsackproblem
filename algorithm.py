9698beb271c06191bb73ac711c8cda476f2f1809
Maciej Bialobrzeski <bialobrzeskimaciek@gmail.com>
2019-02-28 23:35:42 +0100

    end of algoritm. WARNING BUG WITH POPULATION GENES

--- algorithm.py
+++ algorithm.py
@@ -16,6 +16,7 @@ class Algorithm(object):
     items = []
     fitness = []
     next_gen = []
+    generation_solution = []
 
     sack = None
     total_fitness_of_generation = 0
@@ -24,8 +25,53 @@ class Algorithm(object):
         self.sack = sack
         self.items = items
 
+    def performAlgorithm(self):
+        self.maximum_generations = self.population_size
+
+        self.generatePopulation()
+
+        self.evaluateFitness()
+        print("\n Fitness: ")
+        for i in range(0, self.population_size-1):
+            print(i+1, "-", self.fitness[i])
+        
+        while(self.maximum_generations>= 0):
+            self.evaluateFitness()
+            
+            for i in range(0, int(self.population_size-1/2)):
+                if(self.population_size % 2 == 1):
+                    self.next_gen.append(self.generation_solution[self.generation_counter -1])
+                
+                gene1 = self.selectGene()
+                gene2 = self.selectGene()
+
+                self.crossoverGenes(gene1, gene2)
+
+            self.evaluateFitness()
+
+            for i in range(0, self.population_size-1):
+                print("#",i+1, " ", self.next_gen[i])
+                self.population[i] = self.next_gen[i]
+                # self.population.insert(i, self.next_gen[i])
+            
+            print("\n Fitness: ")
+            for m in range(0, self.population_size-1):
+                print(m+1, " - ", self.fitness[m])
+            
+            self.next_gen.clear()
+            self.fitness.clear()
+
+            print("Crossover occurred ", self.crossover_count, " times.")
+            print("Cloning occurred ", self.clone_count, " times.")
+            
+            if(self.clone_count==0):
+                print("Mutation did not occur \n")
+            else:
+                print("Mutation did occur \n")
+            
+            self.maximum_generations -= 1
+
     def selectGene(self):
-        print('test')
         rand = random.uniform(0, self.total_fitness_of_generation)
         
         for i in range(0,self.population_size):
@@ -40,7 +86,33 @@ class Algorithm(object):
             mut_gene = ""
             new_mut_gene = ""
             mut_point = 0
-    
+            which_gene = random.random()
+
+            if(which_gene <= 0.5):
+                mut_gene = self.next_gen[len(self.next_gen) - 1]
+                mut_point = random.randint(0, len(self.items)-2)
+                
+                if(mut_gene[mut_point:mut_point + 1] == "1"):
+                    new_mut_gene = mut_gene[0:mut_point] + "0" + mut_gene[mut_point + 1]
+                    self.next_gen[len(self.next_gen)-1] = new_mut_gene
+                
+                if(mut_gene[mut_point:mut_point + 1] == "0"):
+                    new_mut_gene = mut_gene[0:mut_point] + "1" + mut_gene[mut_point + 1]
+                    self.next_gen[len(self.next_gen)-1] = new_mut_gene
+                    # self.next_gen.insert(len(self.next_gen)-1, new_mut_gene)
+            
+            if(which_gene > 0.5):
+                mut_gene = self.next_gen[len(self.next_gen) - 2]
+                mut_point = random.randint(0, len(self.items)-2)
+
+                if(mut_gene[mut_point:mut_point + 1] == "1"):
+                    new_mut_gene = mut_gene[0:mut_point] + "0" + mut_gene[mut_point + 1]
+                    self.next_gen[len(self.next_gen)-1] = new_mut_gene 
+
+                if(mut_gene[mut_point:mut_point + 1] == "0"):
+                    new_mut_gene = mut_gene[0:mut_point] + "1" + mut_gene[mut_point + 1]
+                    self.next_gen[len(self.next_gen)-2] = new_mut_gene
+
     def crossoverGenes(self, gene1, gene2):
         new_gene1 = ""
         new_gene2 = ""
@@ -48,15 +120,16 @@ class Algorithm(object):
         rand = random.random()
         if(rand <= self.prob_crossover):
             self.crossover_count += 1
-            cross_point = random.uniform(1, self.items.len)
+            cross_point = random.randint(0, len(self.items)-2)
+            # cross_point = random.uniform(1, len(self.items))
 
             new_gene1 = self.population[gene1][0:cross_point] + self.population[gene2][cross_point]
             new_gene2 = self.population[gene2][0:cross_point] + self.population[gene1][cross_point]
 
-            self.next_gen.append(new_gene1, new_gene2)
+            # self.next_gen.extend((new_gene1, new_gene2))
         else:
             self.clone_count += 1
-            self.next_gen.append(self.population[gene1], self.population[gene2])
+            # self.next_gen.extend((self.population[gene1], self.population[gene2]))
 
         self.mutateGen()
 
@@ -69,8 +142,7 @@ class Algorithm(object):
                 if (random.random() > 0.5):
                     gene += "1"
                 else: gene += "0"
-
-        self.population.append(gene)
+            self.population.append(gene)
     
     def evaluateFitness(self):
 
@@ -78,19 +150,18 @@ class Algorithm(object):
         fitest = 0
         fitest_index = 0
 
-        for i in range(0, self.population_size):
+        for i in range(0, self.population_size-1):
             total_weight = 0
             total_value = 0
             fitness_value = 0
             diff = 0
-
-            for j in range(0, self.sack.sack_items_count):
-                if (self.population[j] == '1'):
+            for j in range(0, self.sack.sack_items_count-1):
+                if (self.population[i][j] == '1'):
                     item = self.items[j]
                     total_weight += item.weight
                     total_value += item.value
 
-            diff = self.sack.capacity - total_weight
+            diff = self.sack.max_capacity - total_weight
             if (diff >= 0):
                 fitness_value = total_value
             
@@ -100,3 +171,9 @@ class Algorithm(object):
                 fitest_index = j
 
             self.total_fitness_of_generation += fitness_value
+
+        print("Total Generation Fitness : ", self.total_fitness_of_generation)
+        print("The fittest chromosome of this generation is ", self.population[fitest_index])
+        print(" And its fitness is ", self.fitness[fitest_index])
+            
+        self.generation_solution.append(self.population[fitest_index])
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from Abstract import sack
 from Abstract import item
-import random
 
+import random
+import algorithm
 random.seed()
 # import algorithm
 
 def main():
     s1 = sack.Sack()
-    print(s1.max_capacity)
-    items = []
-    i1 = item.Item('Test', 5, 10)
-    items = {'1': i1}
+    
+    i1 = item.Item('Test1', 5, 10)
+    i2 = item.Item('Test2', 2, 1)
+    i3 = item.Item('Test3', 10, 30)
+    i4 = item.Item('Test4', 8, 12)
+    i5 = item.Item('Test5', 3, 3)
 
-    print(random.random())
+    items = []
+    items.extend((i1, i2, i3, i4, i5))
+
+    a1 = algorithm.Algorithm(s1, items)
+
+    a1.performAlgorithm()
 
 if __name__ == "__main__":
     main()
